3b6942e5e1ad4f07b6250a66fe19658de2f6e0ca
Alexander Sklar <asklar@microsoft.com>
2025-07-31 16:00:53 -0700

    figma sample

---
+++ src/ModelContextProtocol/Protocol/Transport/NamedPipeServerTransport.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using System.IO.Pipes;
+
+namespace ModelContextProtocol.Protocol.Transport;
+
+/// <summary>
+/// Provides an <see cref="ITransport"/> implemented via named pipes.
+/// </summary>
+public class NamedPipeServerTransport : StreamServerTransport
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NamedPipeServerTransport"/> class with explicit input/output streams.
+    /// </summary>
+    /// <param name="pipeName"></param>
+    /// <param name="serverName"></param>
+    /// <param name="loggerFactory"></param>
+    public NamedPipeServerTransport(string pipeName, string? serverName = null, ILoggerFactory? loggerFactory = null)
+        : base(new NamedPipeServerStream(pipeName), new NamedPipeClientStream(pipeName), serverName, loggerFactory)
+    {
+        PipeName = pipeName ?? throw new ArgumentNullException(nameof(pipeName));
+    }
+
+    /// <summary>
+    /// The name of the named pipe used for communication.
+    /// </summary>
+    public string PipeName { get; }
+}
